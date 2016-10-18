a95970cbc25d410ef71bf2d179095f7e2eaa7cf6
Velu <velu_developer@yahoo.com>
2016-10-18 09:54:31 +0530

    Initial seed setup

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+
+	entry: {
+		'app': './src/main.ts',
+		'polyfills': [
+			'core-js/es6',
+			'core-js/es7/reflect',
+			'zone.js/dist/zone'
+		]
+	},
+	output: {
+		path: './dist',
+		filename: '[name].[hash].js'
+	},
+	module: {
+		loaders: [
+			{test: /\.component.ts$/, loader: 'ts!angular2-template'},
+			{test: /\.ts$/, exclude: /\.component.ts$/, loader: 'ts'},
+			{test: /\.html$/, loader: 'raw'},
+			{test: /\.css$/, loader: 'raw'}
+		]
+	},
+	resolve: {
+		extensions: ['', '.js', '.ts', '.html', '.css']
+	},
+	plugins: [
+		new webpack.optimize.CommonsChunkPlugin({
+			name: 'polyfills'
+		}),
+		new HtmlWebpackPlugin({
+			template: './src/index.html'
+		}),
+		new webpack.DefinePlugin({
+			app: {
+				environment: JSON.stringify(process.env.APP_ENVIRONMENT || 'development')
+			}
+		})
+	]
+	
+};
